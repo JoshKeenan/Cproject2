9f8dac0aeabe1e89aca4c8b061c681a2d3a3e463
User <user@Users-MacBook-Pro.local>
2017-11-04 19:05:51 +0000

    More functions added, added switch, fix double question

--- main.c
+++ main.c
@@ -1,53 +1,113 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void enqueue(int n){
+char input[1];
+int quit = 1;
+
+void enqueue(int n){                // add patient to the queue
     printf("%i, enqueued\n", n);
 }
 
-int dequeue() {
+int dequeue() {                     // remove patient from the queue
     printf("dequeue\n");
     return 0;
 }
 
-int first(){
+void queue(){
+    printf("Queue\n");
+}
+
+int first(){                        // get first patient in the queue(pos)
     printf("First\n");
     return 0;
 }
 
-int last(){
+int last(){                         // get last patient in the queue(pos)
     printf("last\n");
     return 0;
 }
 
-void clear(){
+void clear(){                       // clear queue
     printf("clear\n");
 }
 
-bool isEmpty(){
+bool isEmpty(){                     // state if queue is empty
     printf("isEmpty\n");
     return true;
 }
 
-bool isFull(){
+bool isFull(){                      // state if queue is full
     printf("isFull\n");
     return true;
 }
 
-void print(){
+void print(){                       // list available doctors
     printf("print\n");
 }
 
-int position(int n){
+void help(){                        // display help list with commands
+    printf("help\n");
+}
+
+void ready(){                       // param could be used, ready doctor accept patient
+    printf("ready\n");
+}
+
+int position(int n){                // return position in queue
     printf("position\n");
     return n;
 }
 
-void remover(int n){
+void remover(int n){                // remove patient from queue
     printf("%i, removed\n", n);
 }
 
+void away(){
+    printf("away\n");
+}
+
+void exit(){
+    printf("exit");
+    quit = 0;
+}
+
 int main(){
-    printf("Hello, World!\n");
+    while(quit != 0) {
+        printf("What would you like to do?\n");
+        scanf("%c", &input);
+        switch(input[0]) {
+            case 'i':
+                enqueue(0);
+                break;
+            case 'p':
+                position(0);
+                break;
+            case 'q':
+                remover(0);
+                break;
+            case 'd':
+                print();
+                break;
+            case 'h':
+                help();
+                break;
+            case 'o':
+                dequeue();
+                break;
+            case 'r':
+                ready();
+                break;
+            case 'a':
+                away();
+                break;
+            case 'w':
+                queue();
+                break;
+            case 'x':
+                exit(0);
+                break;
+
+        }
+    }
     return 0;
 }
