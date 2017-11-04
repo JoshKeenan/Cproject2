b7cb7ed49e684f810ab39f722488d657b59f8ad2
User <user@Users-MacBook-Pro.local>
2017-11-04 17:05:39 +0000

    Added function structure

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
+void enqueue(int n){
+    printf("%i, enqueued\n", n);
+}
+
+int dequeue() {
+    printf("dequeue\n");
+    return 0;
+}
+
+int first(){
+    printf("First\n");
+    return 0;
+}
+
+int last(){
+    printf("last\n");
+    return 0;
+}
+
+void clear(){
+    printf("clear\n");
+}
+
+bool isEmpty(){
+    printf("isEmpty\n");
+    return true;
+}
+
+bool isFull(){
+    printf("isFull\n");
+    return true;
+}
+
+void print(){
+    printf("print\n");
+}
+
+int position(int n){
+    printf("position\n");
+    return n;
+}
+
+void remover(int n){
+    printf("%i, removed\n", n);
+}
+
+int main(){
     printf("Hello, World!\n");
     return 0;
 }
