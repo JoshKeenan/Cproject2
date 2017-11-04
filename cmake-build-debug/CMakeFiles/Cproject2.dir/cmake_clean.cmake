a2c5ecf00cf22c643a88f1159223d6cafaf9906a
User <user@Users-MacBook-Pro.local>
2017-11-04 13:16:00 +0000

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Cproject2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cproject2.dir/main.c.o"
+  "Cproject2.pdb"
+  "Cproject2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cproject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
